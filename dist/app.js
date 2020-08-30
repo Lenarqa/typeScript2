a7ed55f1aaa93d9f08b8e58ca9dbf235844c6f29
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2020-08-30 22:45:33 +0700

    add style.css, add generic.ts

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log('app.ts!!');
+console.log('app.ts!!!');
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const max = new Person('Maxim');
+const btn = document.querySelector('#btn');
+if (btn) {
+    btn.addEventListener('click', () => {
+        console.log('btn click');
+    });
+}
+let anyType;
+function showInfo(data) {
+    console.log(data);
+    anyType = true;
+    console.log(anyType);
+}
+function multiple(a, b) {
+    if (a && b) {
+        return a * b;
+    }
+    return;
+}
+showInfo('Hello from show info!');
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
 console.log('app.ts!!!');
+
+class Person{
+    constructor(private name: string){}
+}
+
+const max = new Person('Maxim');
+
+const btn = document.querySelector('#btn');
+if(btn){
+    btn.addEventListener('click',()=>{
+        console.log('btn click');
+        
+    });
+}
+
+let anyType;
+
+function showInfo(data: string): void{
+    console.log(data);
+    anyType = true;
+    console.log(anyType);
+    
+}
+
+function multiple(a: number, b: number){
+    if(a && b){
+        return a * b;
+    }
+    return;
+}
+
+showInfo('Hello from show info!');
