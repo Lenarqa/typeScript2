8965d9d02f786d428913559b4953d181c692a57c
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2020-09-02 23:16:32 +0700

    end with decorators

--- dist/decorators.js
+++ dist/decorators.js
@@ -1,78 +1,41 @@
 "use strict";
+// console.log('decorators');
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-console.log('decorators');
-function Log(construcor) {
-    console.log("Log");
-    console.log(construcor);
+const validators = {};
+function Required(target, propName) {
+    validators[target.construcor.name] = Object.assign(Object.assign({}, validators[target.construcor.name]), { [propName]: 'required' });
 }
-function Log2(target, propName) {
-    console.log("Log2");
-    console.log(target);
-    console.log(propName);
+function Validate(obj) {
+    const objConfig = validators[obj.construcor.name];
+    if (!objConfig) {
+        return true;
+    }
+    let isValid = true;
+    Object.keys(objConfig).forEach(key => {
+        if (objConfig[key] === 'required') {
+            isValid = isValid && !!obj[key];
+        }
+    });
+    return isValid;
 }
-function Log3(target, propName, descriptor) {
-    console.log("Log3");
-    console.log(target);
-    console.log(propName);
-    console.log(descriptor);
+class Form {
+    constructor(email) {
+        this.email = email;
+    }
 }
-let Component = class Component {
-    constructor(name) {
-        this.name = name;
-    }
-    get componentName() {
-        return this.name;
-    }
-    logName() {
-        console.log(`Component name: ${this.name}`);
-    }
-};
 __decorate([
-    Log2
-], Component.prototype, "name", void 0);
-__decorate([
-    Log3
-], Component.prototype, "componentName", null);
-__decorate([
-    Log3
-], Component.prototype, "logName", null);
-Component = __decorate([
-    Log
-], Component);
-function ComponentFun(config) {
-    return function (constructor) {
-        return class extends constructor {
-            constructor(...args) {
-                super(...args);
-                const el = document.querySelector(config.selector);
-                el.innerHTML = config.template;
-            }
-        };
-    };
+    Required
+], Form.prototype, "email", void 0);
+const form = new Form();
+console.log(form);
+if (Validate(form)) {
+    console.log("valid = ", form);
 }
-let Component2 = class Component2 {
-    constructor(name) {
-        this.name = name;
-    }
-    logName() {
-        console.log(`Component2 name = ${this.name}`);
-    }
-};
-Component2 = __decorate([
-    ComponentFun({
-        selector: "#card",
-        template: `
-        <div class="card">
-            <div class="card-content">
-                <h1>Card №1</h1>
-            <div> 
-        <div>
-    `
-    })
-], Component2);
-const card = new Component2('My card component!');
+else {
+    console.log('valid error');
+}
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,81 +1,149 @@
-console.log('decorators');
+// console.log('decorators');
 
-function Log(construcor: Function) {
-    console.log("Log")
-    console.log(construcor);
-}
+// function Log(construcor: Function) {
+//     console.log("Log")
+//     console.log(construcor);
+// }
 
-function Log2(target: any, propName: string | Symbol) {
-    console.log("Log2")
-    console.log(target);
-    console.log(propName);
-}
+// function Log2(target: any, propName: string | Symbol) {
+//     console.log("Log2")
+//     console.log(target);
+//     console.log(propName);
+// }
 
-function Log3(target: any, propName: string | Symbol, descriptor: PropertyDescriptor) {
-    console.log("Log3")
-    console.log(target);
-    console.log(propName);
-    console.log(descriptor);
-}
+// function Log3(target: any, propName: string | Symbol, descriptor: PropertyDescriptor) {
+//     console.log("Log3")
+//     console.log(target);
+//     console.log(propName);
+//     console.log(descriptor);
+// }
 
-@Log
-class Component {
-    @Log2
-    name: string;
+// @Log
+// class Component {
+//     @Log2
+//     name: string;
 
-    constructor(name: string) {
-        this.name = name;
-    }
+//     constructor(name: string) {
+//         this.name = name;
+//     }
 
-    @Log3
-    get componentName() {
-        return this.name;
-    }
+//     @Log3
+//     get componentName() {
+//         return this.name;
+//     }
 
-    @Log3
-    logName(): void {
-        console.log(`Component name: ${this.name}`);
-    }
-}
-// ==================================
-interface ComponentDecorator {
-    selector: string;
-    template: string;
-}
+//     @Log3
+//     logName(): void {
+//         console.log(`Component name: ${this.name}`);
+//     }
+// }
+// // ==================================
+// interface ComponentDecorator {
+//     selector: string;
+//     template: string;
+// }
 
-function ComponentFun(config: ComponentDecorator) {
-    return function 
-    <T extends { new(...args: any[]): object }>
-    (constructor: T) {
-        return class extends constructor {
-            constructor(...args: any[]){
-                super(...args);
+// function ComponentFun(config: ComponentDecorator) {
+//     return function 
+//     <T extends { new(...args: any[]): object }>
+//     (constructor: T) {
+//         return class extends constructor {
+//             constructor(...args: any[]){
+//                 super(...args);
                 
-                const el = document.querySelector(config.selector)!;
-                el.innerHTML = config.template;
-            }
-        }
-    }
-}
+//                 const el = document.querySelector(config.selector)!;
+//                 el.innerHTML = config.template;
+//             }
+//         }
+//     }
+// }
 
+// function Bind(_: any, _2: any, descriptor: PropertyDescriptor): PropertyDescriptor {
+//     const original = descriptor.value;
 
-@ComponentFun({
-    selector: "#card",
-    template: `
-        <div class="card">
-            <div class="card-content">
-                <h1>Card №1</h1>
-            <div> 
-        <div>
-    `
-})
-class Component2 {
-    constructor(public name: string) {}
+//     return {
+//         configurable: true,
+//         enumerable: false,
+//         get() {
+//             return original.bind(this)
+//         }
+//     }
+// }
 
-    logName() {
-        console.log(`Component2 name = ${this.name}`);
+// @ComponentFun({
+//     selector: "#card",
+//     template: `
+//         <div class="card">
+//             <div class="card-content">
+//                 <h1>Card №1</h1>
+//             <div> 
+//         <div>
+//     `
+// })
+
+// class Component2 {
+//     constructor(public name: string) {}
+
+//     @Bind
+//     logName() {
+//         console.log(`Component2 name = ${this.name}`);
         
+//     }
+// }
+
+// const card = new Component2('My card component!');
+
+// const btn2 = document.querySelector('#btn2')!;
+// btn2.addEventListener('click', card.logName)
+
+// ===============
+type ValidatorType = 'required' | 'email';
+
+interface validatorConfig {
+    [prop: string]: {
+        [validatorProm: string]: ValidatorType;        
     }
 }
 
-const card = new Component2('My card component!')
+const validators: validatorConfig = {}
+
+function Required (target: any, propName: string) {
+    validators[target.construcor.name] = {
+        ...validators[target.construcor.name],
+        [propName]: 'required'
+    }
+}   
+
+function Validate(obj: any): boolean {
+    const objConfig = validators[obj.construcor.name];
+    if(!objConfig) {
+        return true;
+    }
+    let isValid = true;
+
+    Object.keys(objConfig).forEach(key => {
+        if(objConfig[key] === 'required') {
+            isValid = isValid && !!obj[key]
+        }
+    });
+
+    return isValid;
+}
+
+class Form {
+    @Required
+    public email: string | void;
+
+    constructor(email?: string) {
+        this.email = email;
+    }
+}
+
+const form = new Form();
+console.log(form);
+
+if(Validate(form)) {
+    console.log("valid = ", form)
+}else{
+    console.log('valid error'); 
+}
